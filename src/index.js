5c2a82a414f060af116ec0b96ae6eba69f42c6a2
albertthewinner2000 <nalbert2012@ya.ru>
2020-10-04 18:29:50 +0300

    feat:task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+exports.min = function min(array) {
+  if (array == undefined || array.length === 0 )  return 0
+  return array.reduce((a, b) => (a > b ? b : a));
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (array == undefined || array.length === 0 )  return 0
+  return array.reduce((a, b) => (a < b ? b : a));
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (array == undefined || array.length === 0 )  return 0
+  return array.reduce((a, b) =>  a + b )/array.length
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+
+
+
